8a89586cef3a3755b82359485db6bdbac5238e92
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-04-08 00:55:37 -0500

    update

--- backend/server/backend_server.py
+++ backend/server/backend_server.py
@@ -1,24 +1,53 @@
 from flask import Flask, request, jsonify
 import jwt
 import datetime
+import os
+import mysql.connector
 
 app = Flask(__name__)
-SECRET_KEY = "clave_secreta"
+
+SECRET_KEY = os.getenv("SECRET_KEY", "clave_secreta")
+
+dbconfig = {
+    "host": os.getenv("MYSQL_HOST"),
+    "port": os.getenv("MYSQL_PORT"),
+    "user": os.getenv("MYSQL_USER"),
+    "password": os.getenv("MYSQL_PASSWORD"),
+    "database": os.getenv("MYSQL_DATABASE_2"),
+}
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=dbconfig["host"],
+        port=dbconfig["port"],
+        user=dbconfig["user"],
+        password=dbconfig["password"],
+        database=dbconfig["database"]
+    )
 
 @app.route('/login/token', methods=['POST'])
 def generate_token():
     data = request.json
+    username = data.get('username')
+    password = data.get('password')
 
-    if not data or data.get('username') != 'admin' or data.get('password') != '1234':
-        return jsonify({'error': 'Invalid credentials'}), 401
-
-    payload = {
-        'user': data['username'],
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-    }
-    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-    return jsonify({'access_token': token})
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required'}), 400
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.callproc('validar_usuario', [username, password])
+        payload = {
+            'user': username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return jsonify({'access_token': token})
+    except mysql.connector.Error as err:
+        return jsonify({'error': str(err)}), 401
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
--- mysql/user.sql
+++ mysql/user.sql
@@ -12,3 +12,17 @@ INSERT INTO users (nombre, contraseña) VALUES ('rodrigo', 'admin');
 INSERT INTO users (nombre, contraseña) VALUES ('angela', 'admin');
 INSERT INTO users (nombre, contraseña) VALUES ('daniel', 'admin');
 INSERT INTO users (nombre, contraseña) VALUES ('esthephany', 'admin');
+
+DELIMITER $$
+CREATE PROCEDURE validar_usuario(IN p_username VARCHAR(255), IN p_password VARCHAR(255))
+BEGIN
+    DECLARE user_exists INT;
+    SELECT 1 INTO user_exists
+    FROM user
+    WHERE username = p_username AND password = p_password
+    LIMIT 1;
+    IF user_exists IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Credenciales no válidas';
+    END IF;
+END $$
+DELIMITER ;
